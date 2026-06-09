7cd21c85b42a3937ceeaf0f9ed0a2c2a0d8da5c2
AnKo <korans1997@mail.ru>
2026-06-09 21:50:42 +0300

    Создать EventDto

---
+++ Ru.AnLadybug.EventManagement/Ru.AnLadybug.EventManagement/ModelDto/EventDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ru.AnLadybug.EventManagement.ModelDto;
+
+public class EventDto
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime StartAt { get; set; }
+    public DateTime EndAt { get; set; }
+}
